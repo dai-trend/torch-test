615ae6d42d1e2297e37932a3fb12a347b9f1c2e2
aitechguy0105 <aitechguy0105@gmail.com>
2025-09-30 17:45:15 +0000

    remove unused ones

--- grail/mining/__init__.py
+++
@@ -1,3 +0,0 @@
-# NOTE: This module is intentionally left empty.
-# It is a placeholder for future miner's CLI refactoring.
-# Please do not remove. Implementation will be added in upcoming updates.
